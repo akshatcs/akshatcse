5a622849a2aed80edcb5bb6abf325229233e65a6
Akshat Kumar <akshatk163@gmail.com>
2025-08-12 15:14:20 +0530

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,59 @@
+import type { IconMap, SocialLink, Site } from '@/types'
+
+export const SITE: Site = {
+  title: 'Akshat Kumar',
+  description:
+    'Building Next-gen Artificial Intelligence, and Occasionally, Why It All Matters.',
+  href: 'https://akshatcse.com',
+  author: 'Akshat Kumar',
+  locale: 'en-US',
+  featuredPostCount: 2,
+  postsPerPage: 3,
+}
+
+export const NAV_LINKS: SocialLink[] = [
+  {
+    href: '/blog',
+    label: 'blog',
+  },
+  {
+    href: '/authors',
+    label: 'authors',
+  },
+  {
+    href: '/about',
+    label: 'about',
+  },
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    href: 'https://twitter.com/akshatcse',
+    label: 'Twitter',
+  },
+  {
+    href: 'https://www.linkedin.com/in/akshatcs',
+    label: 'LinkedIn',
+  },
+  // {
+  //   href: 'mailto:akshat@akshatcse.com',
+  //   label: 'Email',
+  // },
+  {
+    href: 'https://github.com/akshatcs',
+    label: 'GitHub',
+  },
+  {
+    href: '/rss.xml',
+    label: 'RSS',
+  },
+]
+
+export const ICON_MAP: IconMap = {
+  Website: 'lucide:globe',
+  GitHub: 'lucide:github',
+  LinkedIn: 'lucide:linkedin',
+  Twitter: 'lucide:twitter',
+  Email: 'lucide:mail',
+  RSS: 'lucide:rss',
+}
---
+++ src/content/authors/akshat-kumar.md
@@ -0,0 +1,9 @@
+---
+name: 'Akshat Kumar'
+avatar: '/conan_edogawa_profile_pic.jpg'
+bio: 'Developing next-gen AI technologies'
+website: 'https://akshatcse.com'
+twitter: 'https://twitter.com/akshatcse'
+github: 'https://github.com/akshatcs'
+# mail: 'akshat@akshatcse.com'
+---
